4eeb76ef716679274b988fe80e9b71671113c8b7
MadsE <mafreluer@gmail.com>
2026-06-11 09:08:43 +0200

    opdatering
    Har ændret på 2D vektorer og er begyndt med 3D

--- test.py
+++ test.py
@@ -2,12 +2,7 @@ import vektor
 import matplotlib.pyplot as plt
 
 
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
 
-vektor.Vektor2Denhedsvektor(32, 23)
 
-plt.autoscale()
+vektor.Testfunktion3D()
 
-plt.grid()
-plt.show()
--- vektor.py
+++ vektor.py
@@ -7,6 +7,7 @@ def Vektor2Dsum(x1, y1, x2, y2):
     resultX = x1 + x2
     resultY = y1 + y2
     result = plt.arrow(0,0, resultX, resultY, head_width=0.1, head_length=0.1, length_includes_head=True, color="blue")
+    Testfunktion()
     return (resultX, resultY)
 
 
@@ -19,27 +20,29 @@ def Vektor2Dminus(x1, y1, x2, y2):
     v1 = plt.arrow(0,0, x1, y1, head_width=0.1, head_length=0.1, length_includes_head=True, color="green")
     v2 = plt.arrow(x1,y1, a, b, head_width=0.1, head_length=0.1, length_includes_head=True, color="yellow")
     result = plt.arrow(0,0, resultx, resulty, head_width=0.1, head_length=0.1, length_includes_head=True, color="red")
+    Testfunktion()
     return(resultx, resulty)
 
 
 def Vektor2Dskalar(x1, y1, s):
-    x = plt.arrow(0,0, x1 * s, y1 * s, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-    y = plt.arrow(0,0, 0, y1 * s, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-
-    return(x, y)
+    x = x1 * s
+    y = y1 * s
+    v1=plt.arrow(0,0, x, y, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
+    Testfunktion()
+    return(v1)
 
 def Vektor2Dlengde(x1, y1):
     return(math.sqrt(x1**2 + y1**2))
 
 def Vektor2Dvinkel(x1, y1):
-    return (math.degrees(math.atan(y1/x1)))
+    return (math.degrees(math.atan2(y1/x1)))
 
 
 def PolaerKoordinater(x, y):
     r = math.sqrt(x**2 + y**2)
     vinkel = math.atan2(y, x)
     v1= plt.arrow(0,0, x, y, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-
+    Testfunktion()
     return (v1)
 
 
@@ -47,34 +50,79 @@ def Vektor2Dprikprodukt(x1, y1, x2, y2):
     resultx = x1 * x2
     resulty = y1 * y2
     v1=plt.arrow(0,0, resultx, resulty, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
+    Testfunktion()
     return (x1 * x2) + (y1 * y2)
 
 def Vektor2Denhedsvektor(x1, y1):
 
-    
-    v1=plt.arrow(0,0, x1, y1, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-    enhedsvektorX = x1 / Vektor2Dlengde(x1, y1)
-    enhedsvektorY = y1 / Vektor2Dlengde(x1, y1)
-    e1=plt.arrow(0,0, enhedsvektorX, enhedsvektorY, head_width=0.1, head_length=0.1, length_includes_head=True, color="red")
+    lengde = Vektor2Dlengde(x1, y1)
 
+
+    if lengde == 0:
+        return ("Nulvektor har ingen enhedsvektor")
+    
+    enhedsvektorX = x1 / lengde
+    enhedsvektorY = y1 / lengde
+    
+    
+    plt.arrow(0, 0, x1, y1, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
+
+    plt.arrow(0, 0, enhedsvektorX, enhedsvektorY, head_width=0.1, head_length=0.1, length_includes_head=True, color="red")
+
+    Testfunktion()
 
 
     return(enhedsvektorX, enhedsvektorY )
 
 
 def Vektorvinkelimellem2D(x1, y1, x2, y2):
+    
+    prik = Vektor2Dprikprodukt(x1, y1, x2, y2)
+    lengde1 = Vektor2Dlengde(x1, y1)
+    lengde2 = Vektor2Dlengde(x2, y2)
 
-    return(math.acos(Vektor2Dprikprodukt(x1, y1, x2, y2) / (Vektor2Dlengde(x1, y1) * Vektor2Dlengde(x2, y2))))
+    if lengde1 == 0 or lengde2 == 0:
+        return "Man kan ikke finde vinklen med en nulvektor"
 
+    vinkel = math.acos(prik / (lengde1 * lengde2))
+    return math.degrees(vinkel)
+    
 
+#############################################################
+# Vektor3D funktioner
+#############################################################
+fig = None
+ax = None
 
+def plot ():
+    global fig, ax
+    if fig == None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
 
 def Vektor3Dsum(x1, y1, z1, x2, y2, z2):
-    x = plt.arrow(0,0, x1, y1, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-    y = plt.arrow(0,0, x2, y2, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
-    z = plt.arrow(0,0, 0, 0, head_width=0.1, head_length=0.1, length_includes_head=True, color="black")
+    plot()
 
-    return(x, y, z)
+    # Første vektor
+    ax.quiver(0, 0, 0, x1, y1, z1, color="green")
+
+    # Anden vektor starter der hvor første slutter
+    ax.quiver(x1, y1, z1, x2, y2, z2, color="yellow")
+
+    resultX = x1 + x2
+    resultY = y1 + y2
+    resultZ = z1 + z2
+
+    # Resultatvektor
+    ax.quiver(0, 0, 0, resultX, resultY, resultZ, color="blue")
+
+    ax.set_xlim([min(0, x1, x2, resultX), max(0, x1, x2, resultX)])
+    ax.set_ylim([min(0, y1, y2, resultY), max(0, y1, y2, resultY)])
+    ax.set_zlim([min(0, z1, z2, resultZ), max(0, z1, z2, resultZ)])
+
+    Testfunktion3D()
+
+    return (resultX, resultY, resultZ)
 
 
 
@@ -100,12 +148,12 @@ def Vektor3Dlengde(x1, y1, z1):
 
 
 def vektor3Dvinkel(x1, y1, z1):
-    return (math.degrees(math.atan(y1, x1)))
+    return (math.degrees(math.atan2(y1, x1)))
 
 
 def Vektor3DpolaerKoordinater(x, y, z):
     r = math.sqrt(x**2 + y**2 + z**2)
-    vinkel = math.atan(y, x)
+    vinkel = math.atan2(y, x)
     phi = math.acos(z / r)
 
     return (r, vinkel, phi)
@@ -133,3 +181,20 @@ def Krydsprodukt(x1, y1, z1, x2, y2, z2):
     krydsproduktZ = (x1 * y2) - (y1 * x2)
 
     return(krydsproduktX, krydsproduktY, krydsproduktZ)
+
+
+def Testfunktion():
+    plt.autoscale()
+
+    plt.grid()
+    plt.show()
+
+
+def Testfunktion3D():
+    plot()
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    ax.grid()
+    plt.show()
